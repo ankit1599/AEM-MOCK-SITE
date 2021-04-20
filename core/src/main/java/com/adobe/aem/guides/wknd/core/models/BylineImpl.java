7bb53df35e9520c073c35f2652d90f82d40e9c6b
ankit1599 <ankit.tiwari@tothenew.com>
2021-04-20 15:45:08 +0530

    my project

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/BylineImpl.java
@@ -0,0 +1,24 @@
+package com.adobe.aem.guides.wknd.core.models.impl;
+import java.util.List;
+import com.adobe.aem.guides.wknd.core.models.Byline;
+
+public class BylineImpl implements Byline {
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getOccupations() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
